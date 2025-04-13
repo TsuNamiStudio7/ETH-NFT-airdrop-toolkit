2bd37ec16b503ae893e6421fecc96ea0252cb4d9
TsuNamiStudio <xtsunamistudio@gmail.com>
2025-04-13 07:31:20 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+INFURA_URL = "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID"
+NFT_CONTRACT_ADDRESS = "0xYourNFTContractHere"
+ERC20_TOKEN_ADDRESS = "0xYourERC20TokenHere"
+WALLET_PRIVATE_KEY = "your_private_key_here"
+WALLET_ADDRESS = "0xYourWalletAddress"
+GAS_LIMIT = 100000
+GAS_PRICE_GWEI = 30
+AMOUNT_TO_SEND = 10  # in token units (not wei)
